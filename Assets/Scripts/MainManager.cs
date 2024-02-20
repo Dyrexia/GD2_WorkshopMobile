4c0c4560cf558c26efa9e98aae07be82b9ebdec4
Dyrexia <bergier.tristan@gmail.com>
2024-02-20 13:45:32 +0100

    Constructeur, stats, barrel

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 using System.ComponentModel;
 using System.IO;
+using Unity.VisualScripting;
 
 public class MainManager : MonoBehaviour
 {
@@ -13,17 +14,76 @@ public class MainManager : MonoBehaviour
     {
         public List<ZombieData> zombieList = new List<ZombieData>();
         public List<ItemData> ItemList = new List<ItemData>();
+        public List<Barrel> BarrelList = new List<Barrel>();
+        public int InventorySize;
+        public int MaxZombies;
     }
     [Serializable]
     public class ZombieData//classe pour les données des zombies
     {
         public bool IsAway;
         public long ExpectedReturn;
+        public ItemData[] EquippedParts;
+        public string Name;
+        public ZombieData(string name="John") //on initialise le zombie avec le stuff de base
+        { 
+            Name = name;
+            IsAway = false;
+            EquippedParts[0] = new ItemData("Tête");
+            EquippedParts[1] = new ItemData("Bras gauche");
+            EquippedParts[2] = new ItemData("Bras droit");
+            EquippedParts[3] = new ItemData("Torse");
+            EquippedParts[4] = new ItemData("Jambe gauche");
+            EquippedParts[5] = new ItemData("Jambe droite");
+        }
     }
     [Serializable]
     public class ItemData//classe pour tout les données des items
     {
         public string Name;
+        public string Description;
+        public string Bodypart;
+        public bool IsInBarrel;
+        public int SkinIndex;
+        public int Level;
+        public int SizeInInventory;
+        public int Infection;
+        public int Intelligence;
+        public int Power;
+        public int Stealth;
+        public int Durability;
+        public float FermentationModifier;
+        public ItemData(string bodypart,int level=0,int skin=0)//constructeur de la classe : seule la bodypart et le level sont importants, le reste est généré aléatoirement à partir de ces 2 infos
+        {
+            Bodypart = bodypart;
+            Level=level;
+            SkinIndex=skin;
+            IsInBarrel = false;
+            if (Level == 0)//partie de base nulle
+            {
+                Name = Bodypart + " irregardable";
+                Infection = 0;
+                Intelligence = 0;
+                Power = 0;
+                Stealth = 0;
+                Durability = -1;//l'item de base a une DuraInfinie ATTENTION ne doit pas occuper d'espace dans l'inventaire
+                FermentationModifier = 0;
+                SizeInInventory = 0;
+            }
+            else//partie lootée avec un niveau il faut ajouter la gen aléatoire
+            {
+                return;
+            }
+        }
+    }
+    [Serializable]
+    public class Barrel
+    {
+        public float AlcoholStrength;
+        public float Amount;
+        public int Level;
+        public string Type;
+        public ItemData ItemInside;
     }
     public SaveData PlayerData = new SaveData();
 
