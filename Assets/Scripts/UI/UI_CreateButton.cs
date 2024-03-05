6afe622764a4beb4cedb1b9ebe303858b161ffeb
Justine Tristan <justine.tristan@yahoo.com>
2024-03-05 14:00:01 +0100

    UpdateButton

--- Assets/Scripts/UI/UI_CreateButton.cs
+++ Assets/Scripts/UI/UI_CreateButton.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class UI_CreateButton : MonoBehaviour
 {
     public List<ItemData> itemDataRef = new List<ItemData>();
     public Button ButtonBodypart;
-    public Canvas CanvaButton;
+    public Transform CanvaButton;
     public UI_ShowStat UI_ShowStatRef;
+    public float CountTest = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,16 +28,36 @@ public class UI_CreateButton : MonoBehaviour
     
     
     public void CreateButton (string bodypart) 
-    {Debug.Log(MainManager.Instance.PlayerData.ItemList.Count);
-        foreach(ItemData item in MainManager.Instance.PlayerData.ItemList)
+
+    {
+      
+        Debug.Log(MainManager.Instance.PlayerData.ItemList.Count);
+       
+        if (MainManager.Instance != null && MainManager.Instance.PlayerData != null && MainManager.Instance.PlayerData.ItemList != null)
+        
+            foreach (ItemData item in MainManager.Instance.PlayerData.ItemList)
         {
             if (item.Bodypart == bodypart)
                 itemDataRef.Add(item);
+             
+            
             
         }
-       foreach(ItemData item in this .itemDataRef)
-            Debug.Log (item.Bodypart);
-        Button newButton = Instantiate(ButtonBodypart, CanvaButton);
+        foreach (ItemData item in this.itemDataRef)
+        {
+            Debug.Log(item.Bodypart);
+            CountTest++;
+            Debug.Log(CountTest);
+            Button newButton = Instantiate(ButtonBodypart, CanvaButton);
+            newButton.transform.position = new Vector3(CanvaButton.position.x+20000, CanvaButton.position.y+3, CanvaButton.position.z+4);
+           
+          
+          
+            newButton.gameObject.SetActive(true);
+            newButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = bodypart;
+            
+        }
+        //newButton.onClick.AddListener(() => OnButtonClick(bodypart));
     }
         
     
