0b68e278b824995f9f54fdf348b1fc6983cc81fa
Justine Tristan <justine.tristan@yahoo.com>
2024-03-05 10:25:29 +0100

    Update Création bouton

--- Assets/Scripts/UI/UI_CreateButton.cs
+++ Assets/Scripts/UI/UI_CreateButton.cs
@@ -7,6 +7,7 @@ public class UI_CreateButton : MonoBehaviour
 {
     public List<ItemData> itemDataRef = new List<ItemData>();
     public Button ButtonBodypart;
+    public Canvas CanvaButton;
     public UI_ShowStat UI_ShowStatRef;
     // Start is called before the first frame update
     void Start()
@@ -24,14 +25,16 @@ public class UI_CreateButton : MonoBehaviour
     
     
     public void CreateButton (string bodypart) 
-    {
+    {Debug.Log(MainManager.Instance.PlayerData.ItemList.Count);
         foreach(ItemData item in MainManager.Instance.PlayerData.ItemList)
         {
             if (item.Bodypart == bodypart)
                 itemDataRef.Add(item);
+            
         }
        foreach(ItemData item in this .itemDataRef)
             Debug.Log (item.Bodypart);
+        Button newButton = Instantiate(ButtonBodypart, CanvaButton);
     }
         
     
