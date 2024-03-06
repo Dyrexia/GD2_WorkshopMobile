f970ef79d948879e6c11912ce9b3f885a3fd8638
Dyrexia <bergier.tristan@gmail.com>
2024-03-06 17:47:51 +0100

    [WIP] Gen d'item amélioré

--- Assets/Scripts/UI/UI_CreateButton.cs
+++ Assets/Scripts/UI/UI_CreateButton.cs
@@ -11,38 +11,15 @@ public class UI_CreateButton : MonoBehaviour
     public Button ButtonBodypart;
     public Transform CanvaButton;
     public UI_ShowStat UI_ShowStatRef;
-    public float CountTest = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
-    
-    
     public void CreateButton (string bodypart) 
     {
         itemDataRef = MainManager.Instance.PlayerData.ItemLists[bodypart];
-        Debug.Log(itemDataRef[0]);
         foreach (ItemData item in this.itemDataRef)
         {
-            Debug.Log(item.Bodypart);
-            CountTest++;
-            Debug.Log(CountTest);
             Button newButton = Instantiate(ButtonBodypart, CanvaButton);
-           
-          
-          
             newButton.gameObject.SetActive(true);
             newButton.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = bodypart;
-            
         }
         //newButton.onClick.AddListener(() => OnButtonClick(bodypart));
     }    
