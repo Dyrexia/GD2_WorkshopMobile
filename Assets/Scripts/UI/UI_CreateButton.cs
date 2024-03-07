4e7bb3450b99bb10d6aabf8fb2a2d9d2072b2fde
Dyrexia <bergier.tristan@gmail.com>
2024-03-07 12:31:35 +0100

    Debug Sauvegarde

--- Assets/Scripts/UI/UI_CreateButton.cs
+++ Assets/Scripts/UI/UI_CreateButton.cs
@@ -7,19 +7,19 @@ using UnityEngine.UI;
 
 public class UI_CreateButton : MonoBehaviour
 {
-    public List<ItemData> itemDataRef = new List<ItemData>();
+    public List<ItemWrapper> itemDataRef = new List<ItemWrapper>();
     public Button ButtonBodypart;
     public Transform CanvaButton;
     public UI_ShowStat UI_ShowStatRef;
     // Start is called before the first frame update
     public void CreateButton (string bodypart) 
     {
-        itemDataRef = MainManager.Instance.PlayerData.ItemLists[bodypart];
-        foreach (ItemData item in itemDataRef)
+        itemDataRef = MainManager.Instance.PlayerData.ItemLists[bodypart].Items;
+        foreach (ItemWrapper item in itemDataRef)
         {
             Button newButton = Instantiate(ButtonBodypart, CanvaButton);
             Debug.Log("je passe dans la loop");  
-            newButton.GetComponent<UI_ItemButton>().Initialize(item);
+            newButton.GetComponent<UI_ItemButton>().Initialize(item.ItemData);
             newButton.gameObject.SetActive(true);
         }
         //newButton.onClick.AddListener(() => OnButtonClick(bodypart));
