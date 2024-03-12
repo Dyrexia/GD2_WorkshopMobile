071ffead756a3716e82890766c6fb5645b7e3368
Justine Tristan <103434279+LeTristanJ13@users.noreply.github.com>
2024-03-12 10:41:24 +0100

    MenuZombie

--- Assets/Scripts/UI/UI_ConfirmationDelete.cs
+++ Assets/Scripts/UI/UI_ConfirmationDelete.cs
@@ -7,4 +7,23 @@ public class UI_ConfirmationDelete : MonoBehaviour
 {
     public Button yesButton;
     public Button noButton;
+    //public bool IsConfirmed()
+    //{ 
+
+    //}
+
+    private void Start()
+    {
+
+        yesButton.onClick.AddListener(ok);
+
+    }
+
+    private void ok ()
+    {
+        Debug.Log("ok");
+
+    }
 }
+
+
