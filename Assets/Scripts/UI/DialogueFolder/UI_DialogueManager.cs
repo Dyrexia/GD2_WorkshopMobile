4acac230ae88d675d1884684e354efe51c4023da
Justine Tristan <justine.tristan@yahoo.com>
2024-03-16 11:18:50 +0100

    UpdateTutorielDialogue

--- Assets/Scripts/UI/DialogueFolder/UI_DialogueManager.cs
+++ Assets/Scripts/UI/DialogueFolder/UI_DialogueManager.cs
@@ -9,7 +9,10 @@ public class UI_DialogueManager : MonoBehaviour
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI DialogueText;
     private Queue<string> sentences;
-    private float letterSpeed = 0.02f;
+    private float letterSpeed = 0.05f;
+   [SerializeField] private string currentPhrase;
+   [SerializeField] private bool PhraseComplete;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -31,30 +34,58 @@ public class UI_DialogueManager : MonoBehaviour
         DisplayNextPhrase();
 
     }
+
+    public void PhraseVerification ()
+    {   
+
+        if (PhraseComplete is true)
+        {
+           
+            DisplayNextPhrase ();
+
+        }
+
+        else
+        {
+            StopAllCoroutines();
+            DialogueText.text = currentPhrase;
+            Debug.Log("j'accèlere");
+            PhraseComplete = true;
+
+         }
+
+    }
   public void DisplayNextPhrase ()
     {
-
+        PhraseComplete = false;
+       
         if (sentences.Count == 0) 
         {
             EndDialogue();
             return;
         
         }
-        string Phrase =sentences.Dequeue();
+      
+        string Phrase = sentences.Dequeue();
         DialogueText.text = Phrase;
+        currentPhrase = Phrase;
         StopAllCoroutines();
         StartCoroutine(TypePhrase(Phrase, letterSpeed));
+
     }
 
     IEnumerator TypePhrase (string Phrase, float letterSpeed)
     {
+        PhraseComplete = false;
         DialogueText.text = "";
+        
         foreach (char letter in Phrase.ToCharArray()) 
         { 
         DialogueText.text += letter;
-            yield return new WaitForSeconds(letterSpeed);
-            
+            yield return new WaitForSeconds(letterSpeed);  
         }
+        PhraseComplete= true;
+        Debug.Log(PhraseComplete);
     }
     public void EndDialogue ()
     {
