4a29c96ecea4743fffca004e4860030e18bb1af1
Dyrexia <bergier.tristan@gmail.com>
2024-03-20 15:15:29 +0100

    Update des skin + Génération des skin et description

--- Assets/Scripts/Data_Classes/ItemData.cs
+++ Assets/Scripts/Data_Classes/ItemData.cs
@@ -34,11 +34,12 @@ public class ItemData//classe pour tout les données des items
     public int Stealth;//max = 60
     public int Durability = 100;
     public int FermentationModifier=0;
-    public ItemData(string bodypart, int level = 0, string skin = "Feminin")//constructeur de la classe : seule la bodypart et le level sont importants, le reste est généré aléatoirement à partir de ces 2 infos
+    public ItemData(string bodypart, int level = 0, string skin = "Basique")//constructeur de la classe : seule la bodypart et le level sont importants, le reste est généré aléatoirement à partir de ces 2 infos
     {
         Bodypart = bodypart;
         Level = level;
-        SkinLabel = skin;
+        SkinLabel = GetSkin(level);
+        Description = DescriptionHolder.GetDescription(Bodypart, skin);
         IsInBarrel = false;
         Name = Bodypart + NameChar[level/5];
         GenerateRandomModifier();
@@ -47,6 +48,17 @@ public class ItemData//classe pour tout les données des items
         Power = Mathf.FloorToInt((Mathf.Pow(2.23f, level) / 2) * (1 + (RandomGeneration.x + Bias.x)));//version factorisé
         Stealth = Mathf.FloorToInt(15 * Mathf.Pow(level, (1/5)) * (1+(RandomGeneration.y + Bias.y)));//version factorisé
         SizeInInventory = 1;
+    }
+    private string GetSkin(int level)
+    {
+        string[] SkinList = {"Basique","Feminin","Gangster","Rastafara","PopStar" };
+        if(level == 0) 
+            return "Basique";
+        if (level < 5)
+            return SkinList[UnityEngine.Random.Range(0, 2)];
+        if (level < 10)
+            return SkinList[UnityEngine.Random.Range(1, 3)];
+        return SkinList[UnityEngine.Random.Range(2, 4)];
     }
     private void GenerateRandomModifier()//On génère 3 nombres aléatoires avec cette équation x²+y²+z²= 1. selon le nb de chiffres négatifs on applique des buffs aux positifs
     {
@@ -69,6 +81,102 @@ public class ItemData//classe pour tout les données des items
         }
         return;
 
+    }
+}
+public static class DescriptionHolder
+{
+    public static string GetDescription(string part,string skin)
+    {
+        switch (skin) 
+        {
+            case "Basique":
+                switch (part)
+                {
+                    case "Tête":
+                        return "Cette tête zombie, dénuée de toute intelligence, est le joyau de la couronne des horreurs, le parfait accessoire pour les zombies qui veulent se démarquer dans le monde grotesque des non-morts.";
+                    case "Torse":
+                        return "Ce torse zombie, dépourvu de tout six-pack, est la quintessence de la mode débraillée, le vêtement idéal pour ceux qui veulent faire sensation dans le monde des morts-vivants.";
+                    case "Bras droit":
+                        return "Avec ses ongles écaillés et sa peau décolorée, il semble avoir été emprunté à une vieille marionnette oubliée dans un placard poussiéreux. Ce bras zombie, dépourvu de toute grâce, est le parfait accessoire pour les zombies qui cherchent à impressionner avec leur style débraillé et leur désir insatiable de chair fraîche.\r\n";
+                    case "Bras gauche":
+                        return "Avec ses ongles écaillés et sa peau décolorée, il semble avoir été emprunté à une vieille marionnette oubliée dans un placard poussiéreux. Ce bras zombie, dépourvu de toute grâce, est le parfait accessoire pour les zombies qui cherchent à impressionner avec leur style débraillé et leur désir insatiable de chair fraîche.\r\n";
+                    case "Jambe droite":
+                        return "Cette jambe zombie, aux articulations désarticulées, est le parfait accessoire pour les zombies qui veulent donner l'impression d'avoir toujours un pied dans la tombe, littéralement.";
+                    case "Jambe gauche":
+                        return "Cette jambe zombie, aux articulations désarticulées, est le parfait accessoire pour les zombies qui veulent donner l'impression d'avoir toujours un pied dans la tombe, littéralement.";
+                }
+                break;
+            case "Feminin":
+                switch (part)
+                {
+                    case "Tête":
+                        return "dépourvue de toute élégance, est l'accessoire parfait pour les zombies qui veulent séduire mais qui finissent par ressembler à une caricature de mannequin défilant sur un catwalk de l'horreur.";
+                    case "Torse":
+                        return "les zombies qui veulent séduire mais qui finissent par ressembler à un mannequin de vitrine mal habillé.";
+                    case "Bras droit":
+                        return "une sorcière maladroite, avec des ongles vernis qui semblent avoir survécu à une bataille de nail art.";
+                    case "Bras gauche":
+                        return "une sorcière maladroite, avec des ongles vernis qui semblent avoir survécu à une bataille de nail art.";
+                    case "Jambe droite":
+                        return "une danseuse fatiguée après une soirée en talons aiguilles";
+                    case "Jambe gauche":
+                        return "une danseuse fatiguée après une soirée en talons aiguilles";
+                }
+                break;
+            case "Gangster":
+                switch (part)
+                {
+                    case "Tête":
+                        return "Plus décoiffée que vos plans pour l'avenir !";
+                    case "Torse":
+                        return "Avec son t-shirt troué qui est aussi authentique que vos convictions anarchistes, et des tatouages plus punk que vos slogans de manif : Ce torse est le symbole vivant de la rébellion !";
+                    case "Bras droit":
+                        return "Le bras qui secoue l'apocalypse avec un style décadent. Tatouages écaillés et clous rouillés: c'est ça, la vraie rébellion.\r\n";
+                    case "Bras gauche":
+                        return "Le bras qui secoue l'apocalypse avec un style décadent. Tatouages écaillés et clous rouillés: c'est ça, la vraie rébellion.\r\n";
+                    case "Jambe droite":
+                        return "Avec plus de trous que votre compte en banque après une soirée punk, et des bottes cloutées plus pointues que vos répliques dans un débat, cette jambe sera l’accessoire parfait pour marcher avec style dans la décadence !";
+                    case "Jambe gauche":
+                        return "Avec plus de trous que votre compte en banque après une soirée punk, et des bottes cloutées plus pointues que vos répliques dans un débat, cette jambe sera l’accessoire parfait pour marcher avec style dans la décadence !";
+                }
+                break;
+            case "Rastafara":
+                switch (part)
+                {
+                    case "Tête":
+                        return "une coiffure plus sauvage que vos soirées les plus folles! La stone attitude";
+                    case "Torse":
+                        return "plus décontracté que votre jour de congé!";
+                    case "Bras droit":
+                        return "Ce bras vous raconteras au travers de ses tatouages, des histoires qui vous feront voyager à travers le cosmos.";
+                    case "Bras gauche":
+                        return "Ce bras vous raconteras au travers de ses tatouages, des histoires qui vous feront voyager à travers le cosmos.";
+                    case "Jambe droite":
+                        return "Marchez au rythme de la décontraction!";
+                    case "Jambe gauche":
+                        return "Marchez au rythme de la décontraction!";
+                }
+                break;
+            case "Popstar":
+                switch (part)
+                {
+                    case "Tête":
+                        return "Ce torse zombie, dépourvu de tout charisme, est l'accessoire parfait pour les zombies qui veulent être le roi de la piste mais finissent par être le clown de la soirée.";
+                    case "Torse":
+                        return "Ce torse zombie, dépourvu de tout charisme, est l'accessoire parfait pour les zombies qui veulent être le roi de la piste mais finissent par être le clown de la soirée.";
+                    case "Bras droit":
+                        return "Ce bras zombie, dénué de tout moonwalk, est le parfait accessoire pour les zombies qui veulent faire revivre les années 80 avec une touche de décomposition.";
+                    case "Bras gauche":
+                        return "Ce bras zombie, dénué de tout moonwalk, est le parfait accessoire pour les zombies qui veulent faire revivre les années 80 avec une touche de décomposition.";
+                    case "Jambe droite":
+                        return "Cette jambe zombie, dépourvue de tout rythme, est l'accessoire parfait pour les zombies qui veulent montrer leur côté groove mais qui finissent par ressembler à une parade de marionnettes désarticulées.\r\n";
+                    case "Jambe gauche":
+                        return "Cette jambe zombie, dépourvue de tout rythme, est l'accessoire parfait pour les zombies qui veulent montrer leur côté groove mais qui finissent par ressembler à une parade de marionnettes désarticulées.\r\n";
+                }
+                break;
+        }
+        Debug.Log("ERROR");
+        return "error";
     }
 }
 
