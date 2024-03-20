dde91752d7f843b6cf207df742fbb10d4e6c300f
Justine Tristan <103434279+LeTristanJ13@users.noreply.github.com>
2024-03-20 15:53:30 +0100

    UpdateMenuZombie

--- Assets/Scripts/UI/UI_ZombieStat.cs
+++ Assets/Scripts/UI/UI_ZombieStat.cs
@@ -5,39 +5,25 @@ using UnityEngine;
 
 public class UI_ZombieStat : MonoBehaviour
 {
-    private TextMeshProUGUI Text;
-    private void Awake()
+    public TextMeshProUGUI Level;
+    public TextMeshProUGUI Infection;
+    public TextMeshProUGUI Intelligence;
+    public TextMeshProUGUI Power;
+    public TextMeshProUGUI Stealth;
+    public TextMeshProUGUI Name;
+   
+    
+
+    public void OnEnable()
     {
-        Text = GetComponent<TextMeshProUGUI>();
+        ZombieData zombieRef = MainManager.Instance.PlayerData.zombieList[MainManager.Instance.CurrentZombie];
+        Level.text = zombieRef.GetLevel().ToString();
+        Infection.text = zombieRef.GetInfection().ToString();
+        Intelligence.text = zombieRef.GetInfection().ToString() ;
+        Power.text = zombieRef.GetPower().ToString();
+        Stealth.text = zombieRef.GetStealth().ToString();
+        Name.text = zombieRef.Name;
+
     }
-    public void Start()
-    {
-        showZombieStat();
-    }
-    public void showZombieStat()
-    {
-        Text.text = GetZombieStat(MainManager.Instance.PlayerData.zombieList[MainManager.Instance.CurrentZombie]);
-    }
-    private string GetZombieStat(ZombieData zombie)
-    {
-        int power = 0;
-        int level = 0;
-        int infection = 0;
-        int intelligence = 0;
-        int stealth = 0;
-        foreach (var Item in zombie.EquippedParts)
-        {
-            power += Item.Value.Power;
-            level += Item.Value.Level;
-            infection += Item.Value.Infection;
-            intelligence += Item.Value.Intelligence;
-            stealth += Item.Value.Stealth;
-        }
-        return "Stats" +
-            "\nBeauté : " + level +
-            "\nPuissance : " + power +
-            "\nInfection : " + infection +
-            "\nIntelligence : " + intelligence +
-            "\nDiscrétion : " + stealth;
-    }
+    
 }
